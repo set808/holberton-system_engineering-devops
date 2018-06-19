75ba3379700079d36cb88b51a0ebe2422238baf8
Spencer Taylor <spencertaylor808@gmail.com>
2018-06-19 06:03:04 +0000

    Create 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/from:(.\w+)|to:(.\w+)|flags:(\S+\d+)/).join(" ").squeeze.split.join(",")
+puts ARGV[0].scan(/from:(.\w+)|to:(.\w+)|flags:(\S+\d+)/).join(" ").squeeze(" ").split.join(",")
 
